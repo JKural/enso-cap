9945ce4346cc7c0e809936e3759864440167f82d
Jakub Kural <jakub.kural@student.uj.edu.pl>
2026-06-14 02:45:20 +0200

    Added parameters to fixed point proof and candidate programs

--- programs/find-fixed-point-candidate.cpp
+++ programs/find-fixed-point-candidate.cpp
@@ -2,36 +2,113 @@
 
 using namespace enso;
 
-int main() {
-    double alpha = 1.0;
-    double beta = 3.0;
+struct Config {
+    double a = 1.0;
+    double b = 3.0;
     double kappa = 11.0;
     double tau = 1.2;
+    double x0 = 0.5;
     int p = 100;
     int n = 10;
-    double T0 = 100;
-    double period = 5;
+    double T0 = 100.0;
+    double T = 5.0;
+    int newton_iters = 10;
+    double newton_error = 1e-10;
     std::string output_directory = ".";
-    int init_integration_iters = static_cast<int>(T0 / tau * p);
-    capd::DMap x0map(
-        "var:x;"
-        "fun:x;",
-        n + 1
+    std::string output_name = "plot";
+};
+
+std::string print(Config const& cfg) {
+    std::stringstream sstream;
+    sstream << "a                = " << cfg.a << "\n";
+    sstream << "b                = " << cfg.b << "\n";
+    sstream << "kappa            = " << cfg.kappa << "\n";
+    sstream << "tau              = " << cfg.tau << "\n";
+    sstream << "x0               = " << cfg.x0 << "\n";
+    sstream << "p                = " << cfg.p << "\n";
+    sstream << "n                = " << cfg.n << "\n";
+    sstream << "T0               = " << cfg.T0 << "\n";
+    sstream << "T                = " << cfg.T << "\n";
+    sstream << "newton_iters     = " << cfg.newton_iters << "\n";
+    sstream << "newton_error     = " << cfg.newton_error << "\n";
+    sstream << "output_directory = " << cfg.output_directory << "\n";
+    sstream << "output_name      = " << cfg.output_name << "\n";
+    return sstream.str();
+}
+
+capd::ddeshelper::ArgumentParser
+parse_commandline(char argc, char** argv, Config& cfg) {
+    capd::ddeshelper::ArgumentParser args(argc, argv);
+    args.parse("--a=", cfg.a, "Parameter a");
+    args.parse("--b=", cfg.b, "Parameter b");
+    args.parse("--kappa=", cfg.kappa, "Parameter kappa");
+    args.parse("--tau=", cfg.tau, "Delay");
+    args.parse("--x0=", cfg.x0, "Value of the initial (constant) function");
+    args.parse("--p=", cfg.p, "Number of subintervals per delay");
+    args.parse("--n=", cfg.n, "Order of the method");
+    args.parse("--T0=", cfg.T0, "Initial integration time");
+    args.parse("--T=", cfg.T, "Period");
+    args.parse(
+        "--newton_iters=",
+        cfg.newton_iters,
+        "Maximum number of Newton's method iterations"
     );
-    DElNinoSetup
-        setup(DElNinoParamsVector {alpha, beta, kappa, tau}, p, n, 0, 0, n);
-    DElNinoSolution x0(setup.grid().point(-p), setup.grid().point(0), n, x0map);
+    args.parse(
+        "--newton_error=",
+        cfg.newton_error,
+        "Target error for the Netwon's method"
+    );
+    args.parse("--output_directory=", cfg.output_directory, "Output directory");
+    args.parse("--output_name=", cfg.output_name, "Output name");
+    return args;
+}
+
+int main(int argc, char** argv) {
+    Config cfg;
+    auto args = parse_commandline(argc, argv, cfg);
+    if (args.isHelpRequested()) {
+        std::cout << args.getHelp() << "\n";
+        return 0;
+    }
+    std::cout << print(cfg) << "\n";
+    int init_integration_iters = static_cast<int>(cfg.T0 / cfg.tau * cfg.p);
+    DElNinoSetup setup(
+        DElNinoParamsVector {cfg.a, cfg.b, cfg.kappa, cfg.tau},
+        cfg.p,
+        cfg.n,
+        0,
+        0,
+        cfg.n
+    );
+    DElNinoSolution x0(
+        setup.grid().point(-cfg.p),
+        setup.grid().point(0),
+        cfg.n,
+        capd::DVector {cfg.x0}
+    );
     auto x1 = setup.makeSolution();
     setup.integrate(init_integration_iters, x0, x1);
-    draw(setup, x1, 1, ".", "pre-newton");
+    draw(
+        setup,
+        x1,
+        static_cast<int>(3 * cfg.T / cfg.tau * cfg.p),
+        cfg.output_directory,
+        cfg.output_name + "-pre-newton"
+    );
     auto Nx = newton_improve_periodic_solution(
         setup,
         x0,
-        setup.t(period / tau * p),
-        1e-15,
-        1e-15,
-        100,
+        setup.t(cfg.T / cfg.tau * cfg.p),
+        cfg.newton_error,
+        cfg.newton_error,
+        cfg.newton_iters,
         &std::cout
     );
-    draw(setup, Nx, 1, ".", "post-newton");
+    draw(
+        setup,
+        Nx,
+        static_cast<int>(3 * cfg.T / cfg.tau * cfg.p),
+        cfg.output_directory,
+        cfg.output_name + "-post-newton"
+    );
 }
--- programs/fixed-point-proof.cpp
+++ programs/fixed-point-proof.cpp
@@ -1,45 +1,103 @@
 #include <enso-cap/enso-cap.h>
 
+struct Config {
+    capd::interval a = 1.0;
+    capd::interval b = 3.0;
+    capd::interval kappa = 0.34;
+    capd::interval tau = capd::interval(12) / capd::interval(10);
+    double x0 = 0.0;
+    int p = 60;
+    int n = 2;
+    capd::interval T0 = 100.0;
+    capd::interval T = 2.0;
+    int newton_iters = 10;
+    double newton_error = 1e-10;
+    std::string output_directory = ".";
+    std::string output_name = "fixed-point-proof";
+};
+
+std::string print(Config const& cfg) {
+    std::stringstream sstream;
+    sstream << "a                = " << cfg.a << "\n";
+    sstream << "b                = " << cfg.b << "\n";
+    sstream << "kappa            = " << cfg.kappa << "\n";
+    sstream << "tau              = " << cfg.tau << "\n";
+    sstream << "x0               = " << cfg.x0 << "\n";
+    sstream << "p                = " << cfg.p << "\n";
+    sstream << "n                = " << cfg.n << "\n";
+    sstream << "T0               = " << cfg.T0 << "\n";
+    sstream << "T                = " << cfg.T << "\n";
+    sstream << "newton_iters     = " << cfg.newton_iters << "\n";
+    sstream << "newton_error     = " << cfg.newton_error << "\n";
+    sstream << "output_directory = " << cfg.output_directory << "\n";
+    sstream << "output_name      = " << cfg.output_name << "\n";
+    return sstream.str();
+}
+
+capd::ddeshelper::ArgumentParser
+parse_commandline(char argc, char** argv, Config& cfg) {
+    capd::ddeshelper::ArgumentParser args(argc, argv);
+    args.parse("--a=", cfg.a, "Parameter a");
+    args.parse("--b=", cfg.b, "Parameter b");
+    args.parse("--kappa=", cfg.kappa, "Parameter kappa");
+    args.parse("--tau=", cfg.tau, "Delay");
+    args.parse("--x0=", cfg.x0, "Value of the initial (constant) function");
+    args.parse("--p=", cfg.p, "Number of subintervals per delay");
+    args.parse("--n=", cfg.n, "Order of the method");
+    args.parse("--T0=", cfg.T0, "Initial integration time");
+    args.parse("--T=", cfg.T, "Period");
+    args.parse(
+        "--newton_iters=",
+        cfg.newton_iters,
+        "Maximum number of Newton's method iterations"
+    );
+    args.parse(
+        "--newton_error=",
+        cfg.newton_error,
+        "Target error for the Netwon's method"
+    );
+    args.parse("--output_directory=", cfg.output_directory, "Output directory");
+    args.parse("--output_name=", cfg.output_name, "Output name");
+    return args;
+}
+
 using namespace enso;
 
-int main() {
-    capd::interval alpha(1.0);
-    capd::interval beta(3.0);
-    capd::interval kappa(0.34);
-    capd::interval tau = capd::interval(12) / capd::interval(10);
-    capd::interval period(2);
-    int p = 80;
-    int n = 2;
-    double T0 = 100;
-    std::cout << "a = " << alpha << "\n";
-    std::cout << "b = " << beta << "\n";
-    std::cout << "kappa = " << kappa << "\n";
-    std::cout << "tau = " << tau << "\n";
-    std::cout << "period = " << period << "\n";
-    std::cout << "p = " << p << "\n";
-    std::cout << "n = " << n << "\n";
-    std::string output_directory = ".";
-    int init_integration_iters = static_cast<int>(midpoint(T0 / tau * p));
-    capd::DMap x0map(
-        "var:x;"
-        "fun:x;",
-        n + 1
-    );
+int main(int argc, char** argv) {
+    Config cfg;
+    auto args = parse_commandline(argc, argv, cfg);
+    if (args.isHelpRequested()) {
+        std::cout << args.getHelp() << "\n";
+        return 0;
+    }
+    int init_integration_iters =
+        static_cast<int>(midpoint(cfg.T0 / cfg.tau * cfg.p));
     auto [setup, isetup] =
-        create_setups(alpha, beta, kappa, tau, p, n, 0, 0, 10);
-    DElNinoSolution x0(setup.grid().point(-p), setup.grid().point(0), n, x0map);
+        create_setups(cfg.a, cfg.b, cfg.kappa, cfg.tau, cfg.p, cfg.n, 0, 0, 99);
+    DElNinoSolution x0(
+        setup.grid().point(-cfg.p),
+        setup.grid().point(0),
+        cfg.n,
+        capd::DVector {cfg.x0}
+    );
     auto x1 = setup.makeSolution();
     setup.integrate(init_integration_iters, x0, x1);
     auto Nx = newton_improve_periodic_solution(
         setup,
         x0,
-        setup.t(midpoint(period / tau * p)),
-        1e-15,
-        1e-15,
-        10,
+        setup.t(midpoint(cfg.T / cfg.tau * cfg.p)),
+        cfg.newton_error,
+        cfg.newton_error,
+        cfg.newton_iters,
         &std::cout
     );
-    draw(setup, Nx, midpoint(period / tau * p), ".", "post-newton");
+    draw(
+        setup,
+        Nx,
+        midpoint(cfg.T / cfg.tau * cfg.p),
+        cfg.output_directory,
+        cfg.output_name + "-candidate"
+    );
 
     auto ix0 = cast_to_interval(
         isetup,
@@ -48,7 +106,7 @@ int main() {
         capd::interval(-1, 1) * 3e-3,
         capd::interval(-1, 1) * 1e-0,
         most_significant_eigenvectors_left_orthogonal,
-        setup.grid().point(midpoint(period / tau * p)),
+        setup.grid().point(midpoint(cfg.T / cfg.tau * cfg.p)),
         1,
         &std::cout
     );
@@ -57,7 +115,7 @@ int main() {
         inflate(
             isetup,
             ix0,
-            isetup.grid().point(midpoint(period / tau * p)),
+            isetup.grid().point(midpoint(cfg.T / cfg.tau * cfg.p)),
             C_inv,
             1.05,
             &std::cout
@@ -67,7 +125,7 @@ int main() {
     auto [ix, r1] = split_integrate(
         isetup,
         ix0,
-        isetup.grid().point(midpoint(period / tau * p)),
+        isetup.grid().point(midpoint(cfg.T / cfg.tau * cfg.p)),
         8,
         C_inv
     );
@@ -98,6 +156,12 @@ int main() {
         std::cout << "Proof successfull\n";
     }
 
-    draw(isetup, ix0, 1, ".", "ix0");
-    draw(isetup, ix, 1, ".", "ix");
+    draw(
+        isetup,
+        ix0,
+        1,
+        cfg.output_directory,
+        cfg.output_name + "-starting-set"
+    );
+    draw(isetup, ix, 1, cfg.output_directory, cfg.output_name + "-result-set");
 }
